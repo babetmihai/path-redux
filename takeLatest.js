e15c5d345c7a11f17265b726e36676f4b95e23e6
Mihai Babet <babetmihai@users.noreply.github.com>
2018-10-17 08:57:20 +0300

    Update takeLatest.js

--- takeLatest.js
+++ takeLatest.js
@@ -2,52 +2,26 @@
  takes only the latest promise and rejects pending promises ()
  https://github.com/bjoerge/promise-latest
 */
+
 const takeLatest = (fn) => {
-  let latestPromise
-  let pending
-  let resolve
-  let reject
-  const fulfill = (promise) => (value) => {
-    if (promise === latestPromise) {
-      pending = null
-      resolve(value)
+  let lastReject
+  return (...args) => new Promise((resolve, reject) => {
+    if (lastReject) {
+      lastReject(new Error())
     }
-  }
-
-  const fail = (promise) => (error) => {
-    if (promise === latestPromise) {
-      pending = null
-      reject(error)
-    }
-  }
-
-  return (...args) => {
-    latestPromise = fn(...args)
-    if (pending) {
-      pending = null
-      const promiseError = new Error(409)
-      promiseError.isCancelled = true
-      reject(promiseError)
-    }
-    pending = new Promise((_resolve, _reject) => {
-	    resolve = _resolve
-	    reject = _reject
-	 })
-    latestPromise.then(fulfill(latestPromise)).catch(fail(latestPromise))
-    return pending
-  }
-
-  
+    lastReject = reject
+    fn(...args).then(resolve).catch(reject)
+  })
 }
+
 const consoleTimeout = (time) => new Promise(resolve => {
-	setTimeout(() => {
-		return resolve(time)
-	}, time)
+  setTimeout(() => {
+    return resolve(time)
+  }, time)
 })
 const delay = takeLatest(consoleTimeout)
 
 
-delay(5000).then(console.log).catch((error) => console.log(error.message, error.isCancelled))
-delay(500).then(console.log).catch(() => console.log('x'))
+delay(5000).then(console.log).catch((error) => console.log('CANCELED'))
+delay(500).then(console.log).catch(() => console.log('CANCELED'))
 delay(10).then(console.log)
-
