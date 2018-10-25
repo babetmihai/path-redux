485dc12ba0aec554408f542b7e671b1fe1ec2f7e
Mihai Babet <babetmihai@users.noreply.github.com>
2018-10-25 13:29:13 +0300

    use lodash get

--- pathRedux.js
+++ pathRedux.js
@@ -8,17 +8,14 @@
 import omitBy from 'lodash/omitBy'
 import isNil from 'lodash/isNil'
 
-export const pathSelector = (state = {}, type = '') => {
-  const keys = type.split('/').filter(Boolean)
-  switch (true) {
-    case (keys.length === 0): return state
-    case (keys.length === 1): return state[keys[0]]
-    default: return pathSelector(state[keys[0]], keys.slice(1).join('/'))
-  }
-}
+import omitBy from 'lodash/omitBy'
+import isNil from 'lodash/isNil'
+import isObject from 'lodash/isObject'
+import get from 'lodash/get'
 
-export const pathReducer = (state = {}, { type = '', payload }) => {
-  const keys = type.split('/').filter(Boolean)
+export const pathReducer = (state = {}, action = {}) => {
+  const { type = '', payload } = action
+  const keys = type.split('.').filter(Boolean)
   switch (true) {
     case (keys.length === 0): return payload
     case (keys.length === 1): return omitBy({
@@ -29,7 +26,7 @@ export const pathReducer = (state = {}, { type = '', payload }) => {
       ...state,
       [keys[0]]: pathReducer(state[keys[0]], {
         payload,
-        type: keys.slice(1).join('/')
+        type: keys.slice(1).join('.')
       })
     }
   }
@@ -37,32 +34,34 @@ export const pathReducer = (state = {}, { type = '', payload }) => {
 
 export const storeApi = (store) => ({
   select: (selector) => selector(store.getState()),
-  get: (type, defautValue) => {
-    const value = pathSelector(store.getState(), type)
-    if (value === undefined) {
-      return defautValue
-    } else {
-      return value
-    }
-  },
-  set: (type, payload) => new Promise((resolve) => {
-    store.dispatch({ type, payload })
-    return resolve(payload)
-  }),
-  delete: (type) => new Promise((resolve) => {
-    store.dispatch({ type })
-    return resolve()
-  }),
-  update: (type, payload) => new Promise((resolve) => {
-    const value = pathSelector(store.getState(), type)
-    if (
-      typeof value === 'object' &&
-      typeof payload === 'object'
-    ) {
-      store.dispatch({ type, payload: { ...value, ...payload } })
-    } else {
+  get: (type, defautValue) => get(store.getState(), type, defautValue),
+  set: (type, payload) => new Promise((resolve, reject) => {
+    try {
       store.dispatch({ type, payload })
+      return resolve(payload)
+    } catch (error) {
+      return reject(error)
     }
-    return resolve(payload)
+  }),
+  delete: (type) => new Promise((resolve, reject) => {
+    try {
+      store.dispatch({ type })
+      return resolve()
+    } catch (error) {
+      return reject(error)
+    }
+  }),
+  update: (type, payload) => new Promise((resolve, reject) => {
+    try {
+      const value = get(store.getState(), type)
+      if (isObject(value) && isObject(payload)) {
+        store.dispatch({ type, payload: { ...value, ...payload } })
+      } else {
+        store.dispatch({ type, payload })
+      }
+      return resolve(payload)
+    } catch (error) {
+      return reject(error)
+    }
   })
 })
